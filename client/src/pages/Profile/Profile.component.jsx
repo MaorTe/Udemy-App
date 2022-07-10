32dfa5070301c608430a1e8b5f73d9793bdb5dbb
maorte <maor884@gmail.com>
2022-07-11 01:39:54 +0300

    UserProfilePage is mobile responsive

--- client/src/pages/Profile/Profile.component.jsx
+++ client/src/pages/Profile/Profile.component.jsx
@@ -3,127 +3,134 @@ import React, { useEffect, useRef, useState } from 'react';
 import api from '../../API/api';
 import * as S from './Profile.style';
 const Profile = () => {
-	const [user, setUser] = useState([]);
-	const [selectedFile, setSelectedFile] = useState(null);
-	const fileInput = useRef(null);
-	useEffect(() => {
-		const fetchUser = async () => {
-			try {
-				const token = localStorage.getItem('token');
-				const { data } = await api.get('/users/me', {
-					headers: { Authorization: token },
-				});
-				setUser([data]);
-			} catch (e) {
-				console.log(e.message);
-			}
-		};
-		fetchUser();
-	}, []);
+   const [user, setUser] = useState([]);
+   const [selectedFile, setSelectedFile] = useState(null);
+   const fileInput = useRef(null);
+   useEffect(() => {
+      const fetchUser = async () => {
+         try {
+            const token = localStorage.getItem('token');
+            const { data } = await api.get('/users/me', {
+               headers: { Authorization: token },
+            });
+            setUser([data]);
+         } catch (e) {
+            console.log(e.message);
+         }
+      };
+      fetchUser();
+   }, []);
 
-	const fileUploadHandler = async () => {
-		try {
-			const token = localStorage.getItem('token');
-			const fd = new FormData();
-			fd.append('avatar', selectedFile);
+   const fileUploadHandler = async () => {
+      try {
+         const token = localStorage.getItem('token');
+         const fd = new FormData();
+         fd.append('avatar', selectedFile);
 
-			await axios.post(
-				`/api/users/special/me/avatar`,
-				fd,
-				// {
-				// 	onUploadProgress: (progressEvent) => {
-				// 		console.log(
-				// 			'Upload Progress: ' +
-				// 				Math.round((progressEvent.loaded / progressEvent.total) * 100) +
-				// 				'%'
-				// 		);
-				// 	},
-				// },
-				{
-					headers: {
-						Authorization: token,
-						'Content-Type': 'multipart/form-data',
-					},
-				}
-			);
-			setSelectedFile(null);
-			// setUser((prev) => prev);
-		} catch (e) {
-			console.log(e.message);
-		}
-	};
+         await axios.post(
+            `/api/users/special/me/avatar`,
+            fd,
+            // {
+            // 	onUploadProgress: (progressEvent) => {
+            // 		console.log(
+            // 			'Upload Progress: ' +
+            // 				Math.round((progressEvent.loaded / progressEvent.total) * 100) +
+            // 				'%'
+            // 		);
+            // 	},
+            // },
+            {
+               headers: {
+                  Authorization: token,
+                  'Content-Type': 'multipart/form-data',
+               },
+            },
+         );
+         setSelectedFile(null);
+         // setUser((prev) => prev);
+      } catch (e) {
+         console.log(e.message);
+      }
+   };
 
-	// const deleteFile = async () => {
-	// 	try {
-	// 		const { data } = await api.delete(`/users/me/avatar`, {
-	// 			headers: {
-	// 				Authorization: token.token,
-	// 			},
-	// 		});
-	// 	} catch (e) {
-	// 		console.log(e.message);
-	// 	}
-	// };
-	// <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
-	// console.log(user[0]._id);
-	return (
-		<S.PageContainer>
-			<S.CardWrapper>
-				<S.CardWrapperLeft>
-					<S.ImageBorder>
-						{/* <img
-							src={`/users/${user[0]._id}/avatar?v=${Date.now()}`}
-							alt="user"
-							width="100"
-						/> */}
-						{user.length > 0 && (
-							<img
-								src={
-									!selectedFile
-										? `/users/${user[0]._id}/avatar?v=${Date.now()}`
-										: URL.createObjectURL(selectedFile)
-								}
-								alt=""
-							/>
-						)}
-					</S.ImageBorder>
-				</S.CardWrapperLeft>
-				<S.CardWrapperRight>
-					<S.CardWrapperRightInfo>
-						<S.InfoTitle>User Profile</S.InfoTitle>
-						<S.InfoData>
-							<S.InfoDataContent>
-								{user.map((info) => {
-									return (
-										<div key={info._id}>
-											<S.FileInput
-												type="file"
-												onChange={(e) => setSelectedFile(e.target.files[0])}
-												ref={fileInput}
-											/>
-											<S.UploadBtn onClick={() => fileInput.current.click()}>
-												Upload image
-											</S.UploadBtn>
+   // const deleteFile = async () => {
+   // 	try {
+   // 		const { data } = await api.delete(`/users/me/avatar`, {
+   // 			headers: {
+   // 				Authorization: token.token,
+   // 			},
+   // 		});
+   // 	} catch (e) {
+   // 		console.log(e.message);
+   // 	}
+   // };
+   // <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+   // console.log(user[0]._id);
 
-											<S.SaveBtn onClick={fileUploadHandler}>Save</S.SaveBtn>
+   const UserAvatar = () => (
+      <S.CardWrapperImg>
+         <S.ImageBorder>
+            {/* <img
+						src={`/users/${user[0]._id}/avatar?v=${Date.now()}`}
+						alt="user"
+						width="100"
+					/> */}
+            {user.length > 0 && (
+               <img
+                  src={
+                     !selectedFile
+                        ? `/users/${user[0]._id}/avatar?v=${Date.now()}`
+                        : URL.createObjectURL(selectedFile)
+                  }
+                  alt="avatar"
+               />
+            )}
+         </S.ImageBorder>
+      </S.CardWrapperImg>
+   );
 
-											{/* <button onClick={() => deleteFile()}>delete</button> */}
-											<S.EmailTitle>Name</S.EmailTitle>
-											<S.EmailParagraph>{info.name}</S.EmailParagraph>
-											<S.EmailTitle>Age</S.EmailTitle>
-											<S.EmailParagraph>{info.age}</S.EmailParagraph>
-											<S.EmailTitle>Email</S.EmailTitle>
-											<S.EmailParagraph>{info.email}</S.EmailParagraph>
-											{/* <img src={`data: image/png;base64,${user[0].avatar}`} alt="" /> */}
-										</div>
-									);
-								})}
-							</S.InfoDataContent>
-						</S.InfoData>
-					</S.CardWrapperRightInfo>
-				</S.CardWrapperRight>
-			</S.CardWrapper>
-		</S.PageContainer>
-	);
+   return (
+      <S.PageContainer>
+         <S.PageContent>
+            {window.innerWidth > 650 && UserAvatar()}
+
+            <S.UserProfileCard>
+               <S.UserInfo>
+                  <S.InfoTitle>User Profile</S.InfoTitle>
+                  <S.InfoData>
+                     <S.InfoDataContent>
+                        {user.map((info) => {
+                           return (
+                              <div key={info._id}>
+                                 <S.FileInput
+                                    type="file"
+                                    onChange={(e) => setSelectedFile(e.target.files[0])}
+                                    ref={fileInput}
+                                 />
+                                 {window.innerWidth < 650 && UserAvatar()}
+                                 <S.UploadBtn onClick={() => fileInput.current.click()}>
+                                    Upload image
+                                 </S.UploadBtn>
+
+                                 <S.SaveBtn onClick={fileUploadHandler}>Save</S.SaveBtn>
+
+                                 {/* <button onClick={() => deleteFile()}>delete</button> */}
+                                 <S.EmailTitle>Name</S.EmailTitle>
+                                 <S.EmailParagraph>{info.name}</S.EmailParagraph>
+                                 <S.EmailTitle>Age</S.EmailTitle>
+                                 <S.EmailParagraph>{info.age}</S.EmailParagraph>
+                                 <S.EmailTitle>Email</S.EmailTitle>
+                                 <S.EmailParagraph>{info.email}</S.EmailParagraph>
+                                 {/* <img src={`data: image/png;base64,${user[0].avatar}`} alt="" /> */}
+                              </div>
+                           );
+                        })}
+                     </S.InfoDataContent>
+                  </S.InfoData>
+               </S.UserInfo>
+            </S.UserProfileCard>
+         </S.PageContent>
+      </S.PageContainer>
+   );
 };
 export default Profile;
--- client/src/pages/Profile/Profile.style.jsx
+++ client/src/pages/Profile/Profile.style.jsx
@@ -1,113 +1,147 @@
 import styled from 'styled-components';
 
 export const PageContainer = styled.div`
-	/* background-color: #f3f3f3; */
-	/* transform: scale(1.5); */
-	/* display: flex;
-	justify-content: center;
-	align-items: center;
-	top: 50%; */
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   @media all and (max-width: 650px) {
+      position: unset;
+      top: 0;
+      left: 0;
+      transform: scale(1.5);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+   }
 `;
 
 export const FileInput = styled.input`
-	display: none;
+   display: none;
 `;
 export const SaveBtn = styled.button`
-	padding: 2px 10px;
-	border: none;
-	outline: none;
-	border-radius: 10px;
-	background: #384a68;
-	color: #fff;
-	transition: 0.3s all;
-	width: 120px;
-	margin: 5px 0;
-	&:hover {
-		box-shadow: 0px 0px 8px 0px #333;
-	}
+   padding: 2px 10px;
+   border: none;
+   outline: none;
+   border-radius: 10px;
+   background: #384a68;
+   color: #fff;
+   transition: 0.3s all;
+   width: 120px;
+   margin: 5px 0;
+   &:hover {
+      box-shadow: 0px 0px 8px 0px #333;
+   }
 `;
 export const UploadBtn = styled(SaveBtn)`
-	white-space: pre;
-	width: 120px;
+   white-space: pre;
+   width: 120px;
 `;
-export const CardWrapper = styled.div`
-	/* position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%); */
-	transform: scale(1.7);
-	width: 600px;
-	display: flex;
-	box-shadow: 0 1px 20px 0 rgba(69, 90, 100, 0.08);
+export const PageContent = styled.div`
+   transform: scale(1.7);
+   width: 600px;
+   display: flex;
+   box-shadow: 0 1px 20px 0 rgba(69, 90, 100, 0.08);
+   @media all and (max-width: 650px) {
+      transform: scale(1.2);
+      width: 80%;
+      margin-top: 180px;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column-reverse;
+   }
 `;
 
-export const CardWrapperLeft = styled.div`
-	width: 50%;
-	/* background: linear-gradient(to right, #01a9ac, #01dbdf); */
-	/* background: linear-gradient(to right, #9fadad, #01dbdf); */
-	padding: 30px 25px;
-	border-top-left-radius: 5px;
-	border-bottom-left-radius: 5px;
-	text-align: center;
-	color: #fff;
+// UserAvatar
+export const CardWrapperImg = styled.div`
+   width: 50%;
+   /* background: linear-gradient(to right, #01a9ac, #01dbdf);
+   background: linear-gradient(to right, #9fadad, #01dbdf); */
+   border-top-left-radius: 5px;
+   border-bottom-left-radius: 5px;
+   text-align: center;
+   color: #fff;
+   padding: 30px 25px;
+   @media all and (max-width: 650px) {
+      width: 100%;
+      padding: 3px 2px;
+   }
 `;
 
 export const ImageBorder = styled.div`
-	border-radius: 5px;
-	margin-bottom: 10px;
-	img {
-		border-radius: 50%;
-		width: 200px;
-		height: 200px;
-		object-fit: cover;
-	}
+   border-radius: 5px;
+   margin-bottom: 10px;
+   img {
+      border-radius: 50%;
+      width: 200px;
+      height: 200px;
+      object-fit: cover;
+   }
+   @media all and (max-width: 650px) {
+      margin-bottom: 0;
+      img {
+         width: 150px;
+         height: 150px;
+      }
+   }
 `;
 
+export const CardWrapperImgMobile = styled.div`
+   width: 30%;
+   border-top-left-radius: 5px;
+   border-bottom-left-radius: 5px;
+   text-align: center;
+   color: #fff;
+`;
+
+// UserInfo
 export const UserTitle = styled.h4`
-	margin-bottom: 10px;
+   margin-bottom: 10px;
 `;
 
-export const CardWrapperRight = styled.div`
-	width: 65%;
-	background: #fff;
-	padding: 30px 25px;
-	border-top-right-radius: 5px;
-	border-bottom-right-radius: 5px;
+export const UserProfileCard = styled.div`
+   width: 65%;
+   background: #fff;
+   padding: 30px 25px;
+   border-top-right-radius: 5px;
+   border-bottom-right-radius: 5px;
+   @media all and (max-width: 650px) {
+	   padding: 0:
+   }
 `;
 
-export const CardWrapperRightInfo = styled.div`
-	margin-bottom: 25px;
+export const UserInfo = styled.div`
+   margin-bottom: 25px;
+   @media all and (max-width: 650px) {
+	   margin: 0:
+   }
 `;
 
 export const InfoTitle = styled.h3`
-	margin-bottom: 15px;
-	padding-bottom: 5px;
-	border-bottom: 1px solid #e0e0e0;
-	color: #353c4e;
-	text-transform: uppercase;
-	letter-spacing: 5px;
+   margin-bottom: 15px;
+   padding-bottom: 5px;
+   border-bottom: 1px solid #e0e0e0;
+   color: #353c4e;
+   text-transform: uppercase;
+   letter-spacing: 5px;
 `;
 
 export const InfoData = styled.div`
-	display: flex;
-	justify-content: space-between;
+   display: flex;
+   justify-content: space-between;
 `;
 
 export const InfoDataContent = styled.div`
-	width: 45%;
+   width: 45%;
 `;
 
 export const EmailTitle = styled.h4`
-	color: #353c4e;
-	margin-bottom: 5px;
+   color: #353c4e;
+   margin-bottom: 5px;
 `;
 
 export const EmailParagraph = styled.p`
-	font-size: 13px;
-	margin-bottom: 10px;
-	color: #919aa3;
+   font-size: 13px;
+   margin-bottom: 10px;
+   color: #919aa3;
 `;
