4e3e50f328ac5accbc15be91ecfd098928c6a0a7
maorte <maor884@gmail.com>
2022-09-28 03:00:08 +0300

    render deploy

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// const HOST = window.location.origin;
+// const apiURL = process.env.REACT_APP_API_RENDER_URL;
+// console.log(apiURL);
 const api = axios.create({
-	baseURL: '/api',
+   baseURL: `${process.env.REACT_APP_API_RENDER_URL || ''}/api`,
 });
 
 export default api;
