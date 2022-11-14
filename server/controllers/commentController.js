fc0db289bfc84f9e7e63d68a5d39c947dfcff509
maorte <maor884@gmail.com>
2022-11-15 01:20:26 +0200

    implemented comment controller

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,18 +1,84 @@
-const express = require('express');
-const User = require('../models/user');
-const auth = require('../middleware/auth');
-const multer = require('multer');
-const sharp = require('sharp');
-const router = new express.Router();
+const Comment = require('../models/comment');
+const Video = require('../models/video');
 
-const createUser = async (req, res) => {
-   const user = new User(req.body);
+const addNewComment = async (req, res) => {
    try {
-      await user.save();
-      const token = await user.generateAuthToken();
-      res.status(201).send({ user, token });
+      const comment = await Comment.findOne({
+         videoId: req.body.videoId,
+      }).populate({ path: 'comments.owner', select: 'name' });
+
+      const newComment = {
+         ...comment.owner,
+         content: req.body.content,
+         owner: req.user._id,
+      };
+      comment.comments.unshift(newComment);
+      await comment.save();
+
+      const commentToSend = await Comment.findOne({
+         videoId: req.body.videoId,
+      }).populate({ path: 'comments.owner', select: 'name' });
+
+      res.status(200).send(commentToSend.comments);
    } catch (e) {
-      res.status(400).send(e);
+      console.dir(e);
+      res.status(500).send();
    }
 };
-module.exports = { createUser };
+
+const getVideoComments = async (req, res) => {
+   try {
+      const comment = await Comment.findOne({
+         videoId: req.params.videoId,
+      }).populate({ path: 'comments.owner', select: 'name' });
+
+      res.status(200).send(comment.comments);
+   } catch (e) {
+      res.status(500).send();
+   }
+};
+
+const editComment = async (req, res) => {
+   try {
+      const comment = await Comment.findOne({
+         videoId: req.params.videoId,
+      });
+      const foundComment = comment.comments.findIndex(
+         (el) => el._id.toString() === req.body.commentId,
+      );
+      comment.comments[foundComment].content = req.body.content;
+      await comment.save();
+
+      const commentToSend = await Comment.findOne({
+         videoId: req.params.videoId,
+      }).populate({ path: 'comments.owner', select: 'name' });
+
+      res.status(200).send(commentToSend.comments);
+   } catch (e) {
+      console.dir(e);
+      res.status(500).send();
+   }
+};
+
+const deleteComment = async (req, res) => {
+   try {
+      const comment = await Comment.findOne({
+         videoId: req.params.videoId,
+      });
+
+      const foundComment = comment.comments.findIndex(
+         (el) => el._id.toString() === req.params.commentId,
+      );
+      comment.comments.splice(foundComment, 1);
+      await comment.save();
+
+      const commentToSend = await Comment.findOne({
+         videoId: req.params.videoId,
+      }).populate({ path: 'comments.owner', select: 'name' });
+
+      res.status(200).send(commentToSend.comments);
+   } catch (e) {
+      res.status(500).send();
+   }
+};
+module.exports = { addNewComment, getComment, editComment, deleteComment };
--- server/routers/comment.js
+++ server/routers/comment.js
@@ -3,120 +3,29 @@ const auth = require('../middleware/auth');
 const Comment = require('../models/comment');
 const Video = require('../models/video');
 const router = new express.Router();
-// --------------original
+const commentController = require('../controllers/commentController');
 
-// // add comment
-// router.post('/api/comments/newcomment', auth, async (req, res) => {
-// 	try {
-// 		const comment = await Comment.findOne({ videoId: req.body.videoId });
-// 		const newComment = { content: req.body.content, owner: req.user._id };
-// 		comment.comments.unshift(newComment);
-// 		await comment.save();
-// 		res.send(newComment);
-// 	} catch (e) {
-// 		console.dir(e);
-// 		res.status(500).send();
-// 	}
+router.post('/api/comments/newcomment', auth, commentController.addNewComment);
+router.get('/api/comments/:videoId', auth, commentController.getVideoComments);
+router.patch('/api/comments/:videoId', auth, commentController.editComment);
+router.delete('/api/comments/:videoId/:commentId', auth, commentController.deleteComment);
+
+// -- features to be implemented --
+
+// router.get('/api/users/courses/video/comments', auth, async (req, res) => {
+//    try {
+//       const video = await Video.find({});
+//       await req.query
+//          .populate({
+//             path: 'comments',
+//             populate: { path: 'owner', select: 'name avatar-_id' },
+//          })
+//          .execPopulate();
+//       res.send(req.user.courses);
+//    } catch (e) {
+//       res.status(500).send();
+//    }
 // });
-// -----------------------
-
-// add comment ----------modified
-router.post('/api/comments/newcomment', auth, async (req, res) => {
-	try {
-		const comment = await Comment.findOne({
-			videoId: req.body.videoId,
-		}).populate({ path: 'comments.owner', select: 'name' });
-
-		const newComment = {
-			...comment.owner,
-			content: req.body.content,
-			owner: req.user._id,
-		};
-		comment.comments.unshift(newComment);
-		await comment.save();
-
-		const commentToSend = await Comment.findOne({
-			videoId: req.body.videoId,
-		}).populate({ path: 'comments.owner', select: 'name' });
-
-		res.status(200).send(commentToSend.comments);
-	} catch (e) {
-		console.dir(e);
-		res.status(500).send();
-	}
-});
-
-// get comment
-router.get('/api/comments/:videoId', auth, async (req, res) => {
-	try {
-		const comment = await Comment.findOne({
-			videoId: req.params.videoId,
-		}).populate({ path: 'comments.owner', select: 'name' });
-		res.status(200).send(comment.comments);
-	} catch (e) {
-		res.status(500).send();
-	}
-});
-
-// edit comment
-router.patch('/api/comments/:videoId', auth, async (req, res) => {
-	try {
-		const comment = await Comment.findOne({
-			videoId: req.params.videoId,
-		});
-		const foundComment = comment.comments.findIndex(
-			(el) => el._id.toString() === req.body.commentId
-		);
-		comment.comments[foundComment].content = req.body.content;
-		await comment.save();
-
-		const commentToSend = await Comment.findOne({
-			videoId: req.params.videoId,
-		}).populate({ path: 'comments.owner', select: 'name' });
-		res.status(200).send(commentToSend.comments);
-	} catch (e) {
-		console.dir(e);
-		res.status(500).send();
-	}
-});
-
-// delete comment
-router.delete('/api/comments/:videoId/:commentId', auth, async (req, res) => {
-	try {
-		const comment = await Comment.findOne({
-			videoId: req.params.videoId,
-		});
-
-		const foundComment = comment.comments.findIndex(
-			(el) => el._id.toString() === req.params.commentId
-		);
-		comment.comments.splice(foundComment, 1);
-		await comment.save();
-
-		const commentToSend = await Comment.findOne({
-			videoId: req.params.videoId,
-		}).populate({ path: 'comments.owner', select: 'name' });
-
-		res.status(200).send(commentToSend.comments);
-	} catch (e) {
-		res.status(500).send();
-	}
-});
-
-router.get('/api/users/courses/video/comments', auth, async (req, res) => {
-	try {
-		const video = await Video.find({});
-		await req.query
-			.populate({
-				path: 'comments',
-				populate: { path: 'owner', select: 'name avatar-_id' },
-			})
-			.execPopulate();
-		res.send(req.user.courses);
-	} catch (e) {
-		res.status(500).send();
-	}
-});
 
 // GET /comments?completed=true
 // GET /comments?limit=10&skip=20
