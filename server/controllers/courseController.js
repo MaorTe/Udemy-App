81cdbbd7a202cf59bfb1bfab9c32cea6606c150f
maorte <maor884@gmail.com>
2022-11-15 00:45:50 +0200

    implemented course controller

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -1,18 +1,97 @@
-const express = require('express');
-const User = require('../models/user');
+const Course = require('../models/course');
 const auth = require('../middleware/auth');
-const multer = require('multer');
-const sharp = require('sharp');
-const router = new express.Router();
+const adminAuth = require('../middleware/adminAuth');
 
-const createUser = async (req, res) => {
-   const user = new User(req.body);
+const addNewCourse = async (req, res) => {
+   // const course = new Course(req.body);
+   const course = new Course({
+      ...req.body,
+   });
+
    try {
-      await user.save();
-      const token = await user.generateAuthToken();
-      res.status(201).send({ user, token });
+      await course.save();
+      res.status(201).send(course);
    } catch (e) {
       res.status(400).send(e);
    }
 };
-module.exports = { createUser };
+
+const getCoursesByCategory = async (req, res) => {
+   try {
+      const { tag } = req.query;
+      const courses = await Course.find({ tag });
+      // .populate('videos');
+      res.send(courses);
+   } catch (e) {
+      res.status(500).send();
+   }
+};
+
+const getUserOwnCourse = async (req, res) => {
+   const _id = req.params.id;
+
+   try {
+      //const course = await Course.findById(_id);
+      const course = await Course.findOne({ _id, owner: req.user._id });
+      if (!course) {
+         return res.status(404).send();
+      }
+
+      res.send(course);
+   } catch (e) {
+      res.status(500).send();
+   }
+};
+
+const updateUserOwnCourse = async (req, res) => {
+   const updates = Object.keys(req.body);
+   const allowedUpdates = ['description', 'completed'];
+   const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+   if (!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' });
+   }
+
+   try {
+      //const course = await Course.findById(req.params.id);
+      const course = await Course.findOne({
+         _id: req.params.id,
+         owner: req.user._id,
+      });
+
+      if (!course) {
+         return res.status(404).send();
+      }
+
+      updates.forEach((update) => (course[update] = req.body[update]));
+      await course.save();
+      res.send(course);
+   } catch (e) {
+      res.status(400).send(e);
+   }
+};
+
+const deleteUserOwnCourse = async (req, res) => {
+   try {
+      //const course = await Course.findByIdAndDelete(req.params.id);
+      const course = await Course.findOneAndDelete({
+         _id: req.params.id,
+         owner: req.user._id,
+      });
+
+      if (!course) {
+         res.status(404).send();
+      }
+
+      res.send(course);
+   } catch (e) {
+      res.status(500).send();
+   }
+};
+module.exports = {
+   addNewCourse,
+   getCoursesByCategory,
+   getUserOwnCourse,
+   updateUserOwnCourse,
+   deleteUserOwnCourse,
+};
--- server/routers/course.js
+++ server/routers/course.js
@@ -1,96 +1,15 @@
 const express = require('express');
-const Course = require('../models/course');
 const auth = require('../middleware/auth');
 const adminAuth = require('../middleware/adminAuth');
+const courseController = require('../controllers/courseController');
 const router = new express.Router();
 
-router.post('/api/courses/addcourse', auth, adminAuth, async (req, res) => {
-	// const course = new Course(req.body);
-	const course = new Course({
-		...req.body,
-	});
+router.post('/api/courses/addcourse', auth, adminAuth, courseController.addNewCourse);
+router.get('/api/courses/:tag', courseController.getCoursesByTag);
 
-	try {
-		await course.save();
-		res.status(201).send(course);
-	} catch (e) {
-		res.status(400).send(e);
-	}
-});
-
-router.get('/api/courses/:tag', async (req, res) => {
-	try {
-		const { tag } = req.query;
-		const courses = await Course.find({ tag });
-		// .populate('videos');
-		res.send(courses);
-	} catch (e) {
-		res.status(500).send();
-	}
-});
-
-router.get('/api/courses/:id', auth, async (req, res) => {
-	const _id = req.params.id;
-
-	try {
-		//const course = await Course.findById(_id);
-		const course = await Course.findOne({ _id, owner: req.user._id });
-		if (!course) {
-			return res.status(404).send();
-		}
-
-		res.send(course);
-	} catch (e) {
-		res.status(500).send();
-	}
-});
-
-router.patch('/api/courses/:id', auth, async (req, res) => {
-	const updates = Object.keys(req.body);
-	const allowedUpdates = ['description', 'completed'];
-	const isValidOperation = updates.every((update) =>
-		allowedUpdates.includes(update)
-	);
-
-	if (!isValidOperation) {
-		return res.status(400).send({ error: 'Invalid updates!' });
-	}
-
-	try {
-		//const course = await Course.findById(req.params.id);
-		const course = await Course.findOne({
-			_id: req.params.id,
-			owner: req.user._id,
-		});
-
-		if (!course) {
-			return res.status(404).send();
-		}
-
-		updates.forEach((update) => (course[update] = req.body[update]));
-		await course.save();
-		res.send(course);
-	} catch (e) {
-		res.status(400).send(e);
-	}
-});
-
-router.delete('/api/courses/:id', auth, async (req, res) => {
-	try {
-		//const course = await Course.findByIdAndDelete(req.params.id);
-		const course = await Course.findOneAndDelete({
-			_id: req.params.id,
-			owner: req.user._id,
-		});
-
-		if (!course) {
-			res.status(404).send();
-		}
-
-		res.send(course);
-	} catch (e) {
-		res.status(500).send();
-	}
-});
-
+// ------ to be implemented on client side ------
+router.get('/api/courses/:id', auth, courseController.getUserOwnCourse);
+router.patch('/api/courses/:id', auth, courseController.updateUserOwnCourse);
+router.delete('/api/courses/:id', auth, courseController.deleteUserOwnCourse);
+// ------
 module.exports = router;
