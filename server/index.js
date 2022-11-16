c13ce672bc8f46a2da0fb00d7719d77f606ef25e
maorte <maor884@gmail.com>
2022-11-16 21:25:02 +0200

    more changes

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 const router = new express.Router();
 const port = process.env.PORT || 3001;
 
-// app.use(cors());
-app.use(cors({ origin: true }));
+app.use(cors());
+// app.use(cors({ origin: true }));
 app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/courses', courseRouter);
