94d774bbb5d8bbcafe0efca4d32792d952cf6a6a
Maor Tene <maor884@gmail.com>
2022-12-18 06:49:13 +0200

    more changes to imports

--- client/src/components/accountBox/signupForm.jsx
+++ client/src/components/accountBox/signupForm.jsx
@@ -11,7 +11,7 @@ import {
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { registerUser } from '../../features/auth/authActions';
-import Marginer from './../Marginer/index';
+import Marginer from '../Marginer/Marginer';
 
 export function SignUpForm() {
    const history = useHistory();
--- client/src/pages/SignIn.component.jsx
+++ client/src/pages/SignIn.component.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import AccountBox from '../components/AccountBox';
+import AccountBox from '../components/AccountBox/AccountBox';
 
 const SignUpPageContainer = styled.div`
    width: 100%;
